0d84b3785fafa535c85455901b77af54780f3a9c
Pierre Berard <pierre.berard@maestrano.com>
2015-11-06 09:09:55 +0100

    Basic error handling and add synchronization model use in sync job

--- app/jobs/synchronization_job.rb
+++ app/jobs/synchronization_job.rb
@@ -4,21 +4,32 @@ class SynchronizationJob
 
   def sync(org_uid)
     organization = Organization.find_by_uid(org_uid)
-    last_synchronization = Synchronization.where(organization_id: organization.id, status: 'SUCCESS').order(updated_at: :desc).first
-    connec_client = Maestrano::Connec::Client.new(organization.uid)
-    external_client = Restforce.new :oauth_token => organization.oauth_token,
-      refresh_token: organization.refresh_token,
-      instance_url: organization.instance_url,
-      client_id: ENV['salesforce_client_id'],
-      client_secret: ENV['salesforce_client_secret']
+    Rails.logger.info "Start synchronization, organization=#{organization.uid}"
+    current_synchronization = Synchronization.create(organization_id: organization.id, status: 'RUNNING')
 
-    ENTITIES.each do |entity|
-      entity_class = entity.constantize.new
-      connec_entities = entity_class.get_connec_entities(connec_client, last_synchronization)
-      external_entities = entity_class.get_external_entities(external_client, last_synchronization)
+    begin
+      last_synchronization = Synchronization.where(organization_id: organization.id, status: 'SUCCESS').order(updated_at: :desc).first
+      connec_client = Maestrano::Connec::Client.new(organization.uid)
+      external_client = Restforce.new :oauth_token => organization.oauth_token,
+        refresh_token: organization.refresh_token,
+        instance_url: organization.instance_url,
+        client_id: ENV['salesforce_client_id'],
+        client_secret: ENV['salesforce_client_secret']
 
-      entity_class.push_entities_to_connec(connec_client, external_entities, organization)
-      entity_class.push_entities_to_external(external_client, connec_entities, organization)
+      ENTITIES.each do |entity|
+        entity_class = entity.constantize.new
+        connec_entities = entity_class.get_connec_entities(connec_client, last_synchronization)
+        external_entities = entity_class.get_external_entities(external_client, last_synchronization)
+
+        entity_class.push_entities_to_connec(connec_client, external_entities, organization)
+        entity_class.push_entities_to_external(external_client, connec_entities, organization)
+      end
+
+      Rails.logger.info "Finished synchronization, organization=#{organization.uid}, status=success"
+      current_synchronization.update_attributes(status: 'SUCCESS')
+    rescue Exception => e
+      Rails.logger.info "Finished synchronization, organization=#{organization.uid}, status=error, message=#{e.message} backtrace=#{e.backtrace.join("\n\t")}"
+      current_synchronization.update_attributes(status: 'ERROR', message: e.message)
     end
   end
 end
