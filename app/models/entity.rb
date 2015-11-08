a0a44b10582c454e2cdac9abbb4556f0fe45bc0a
Pierre Berard <pierre.berard@maestrano.com>
2015-11-08 14:40:33 +0100

    fix

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -47,6 +47,7 @@ class Entity
       entities = client.query("select Id, LastModifiedDate, #{fields} from #{self.external_entity_name} ORDER BY LastModifiedDate DESC")
       entities = entities.to_a
 
+      Rails.logger.info "Source=#{@@external_name}, Entity=#{self.external_entity_name}, Response=#{entities}"
       index = entities.find_index{|entity| entity.LastModifiedDate < last_synchronization.updated_at }
       entities = index ? (index >= 0 ? entities[0..index] : []) : entities
       Rails.logger.info "Source=#{@@external_name}, Entity=#{self.external_entity_name}, Response=#{entities}"
