982ee033454b923556230b98b649879943219ec5
Pierre Berard <pierre.berard@maestrano.com>
2015-11-08 15:15:20 +0100

    Extend account sync to all fields

--- app/models/orga.rb
+++ app/models/orga.rb
@@ -17,10 +17,25 @@ class Orga < Entity
   end
 
   def external_attributes
-    [
-      "Name",
-      "Industry"
-    ]
+    %w(
+      Name
+      Industry
+      AnnualRevenue
+      NumberOfEmployees
+      BillingStreet
+      BillingCity
+      BillingState
+      BillingPostalCode
+      BillingCountry
+      ShippingStreet
+      ShippingCity
+      ShippingState
+      ShippingPostalCode
+      ShippingCountry
+      Website
+      Phone
+      Fax
+    )
   end
 
 end
@@ -30,4 +45,23 @@ class OrgaMapper
 
   map from('/name'),  to('/Name')
   map from('/industry'),  to('/Industry')
+  map from('/annual_revenue'), to('/AnnualRevenue')
+  map from('/number_of_employees'), to('/NumberOfEmployees')
+
+  map from('/address/billing/line1'), to('/BillingStreet')
+  map from('/address/billing/city'), to('/BillingCity')
+  map from('/address/billing/region'), to('/BillingState')
+  map from('/address/billing/postal_code'), to('/BillingPostalCode')
+  map from('/address/billing/country'), to('/BillingCountry')
+
+  map from('/address/shipping/line1'), to('/ShippingStreet')
+  map from('/address/shipping/city'), to('/ShippingCity')
+  map from('/address/shipping/region'), to('/ShippingState')
+  map from('/address/shipping/postal_code'), to('/ShippingPostalCode')
+  map from('/address/shipping/country'), to('/ShippingCountry')
+
+  map from('/website/url'), to('/Website')
+  map from('/phone/landline'), to('/Phone')
+  map from('/phone/fax'), to('/Fax')
 end
+
