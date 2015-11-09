5da9e5c7b5e8c1ccd26e8ab69d5e44c2f8865f5f
Pierre Berard <pierre.berard@maestrano.com>
2015-11-09 18:54:57 +0100

    Add opts to sync job

--- app/jobs/synchronization_job.rb
+++ app/jobs/synchronization_job.rb
@@ -2,7 +2,7 @@ class SynchronizationJob
   ENTITIES = %w(Organization Person)
   EXTERNAL_NAME = "SalesForce"
 
-  def sync(org_uid)
+  def sync(org_uid, opts={})
     organization = Organization.find_by_uid(org_uid)
     Rails.logger.info "Start synchronization, organization=#{organization.uid}"
     current_synchronization = Synchronization.create(organization_id: organization.id, status: 'RUNNING')
@@ -18,10 +18,10 @@ class SynchronizationJob
 
       ENTITIES.each do |entity|
         entity_class = "Entities::#{entity}".constantize.new
-        connec_entities = entity_class.get_connec_entities(connec_client, last_synchronization)
-        external_entities = entity_class.get_external_entities(external_client, last_synchronization)
+        connec_entities = entity_class.get_connec_entities(connec_client, last_synchronization, opts)
+        external_entities = entity_class.get_external_entities(external_client, last_synchronization, opts)
 
-        entity_class.consolidate_and_map_data(connec_entities, external_entities, organization, opts={})
+        entity_class.consolidate_and_map_data(connec_entities, external_entities, organization, opts)
 
         entity_class.push_entities_to_connec(connec_client, external_entities, organization)
         entity_class.push_entities_to_external(external_client, connec_entities, organization)
