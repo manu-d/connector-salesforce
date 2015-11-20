55dfe0a9f9ae2f2e2676a68c7be697cb157f8625
Pierre Berard <pierre.berard@maestrano.com>
2015-11-20 17:49:07 +0100

    Re-switches to Connec! sync done each time

--- app/jobs/synchronization_job.rb
+++ app/jobs/synchronization_job.rb
@@ -5,8 +5,7 @@ class SynchronizationJob < Struct.new(:organization, :opts)
   #  * :forced => true  synchronization has been triggered manually (for logging purposes only)
   #  * :only_entities => [person, tasks_list]
   #  * :full_sync => true  synchronization is performed without date filtering
-  #  * :force_sync_connec => true  Force a fetching of Connec! data (usually only done for the first sync, subsequents syncs are done via webhooks)
-  #  * :external_preemption => true  preemption is given to external instead of connec! is case of conflict. Usefull only is synchronization is performed to both connec! and external
+  #  * :external_preemption => true  preemption is given to external instead of connec! is case of conflict
   def perform
     Rails.logger.info "Start synchronization, organization=#{organization.uid} #{opts[:forced] ? 'forced=true' : ''}"
     current_synchronization = Synchronization.create(organization_id: organization.id, status: 'RUNNING')
@@ -47,15 +46,9 @@ class SynchronizationJob < Struct.new(:organization, :opts)
       entity_class.set_mapper_organization(organization.id)
 
       external_entities = entity_class.get_external_entities(external_client, last_synchronization, opts)
-
-      if last_synchronization.blank? || opts[:force_sync_connec]
-        connec_entities = entity_class.get_connec_entities(connec_client, last_synchronization, opts)
-        entity_class.consolidate_and_map_data(connec_entities, external_entities, organization, opts)
-        entity_class.push_entities_to_external(external_client, connec_entities, organization)
-      else
-        entity_class.map_external_entities(external_entities)
-      end
-
+      connec_entities = entity_class.get_connec_entities(connec_client, last_synchronization, opts)
+      entity_class.consolidate_and_map_data(connec_entities, external_entities, organization, opts)
+      entity_class.push_entities_to_external(external_client, connec_entities, organization)
       entity_class.push_entities_to_connec(connec_client, external_entities, organization)
 
       entity_class.unset_mapper_organization
