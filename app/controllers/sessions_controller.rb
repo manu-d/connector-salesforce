d4edb176e576af184c61011557f1673238b90691
Pierre Berard <pierre.berard@maestrano.com>
2015-11-21 10:38:46 +0100

    Adds session delete to logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -45,6 +45,8 @@ class SessionsController < ApplicationController
   # Logout
   def destroy
     session.delete(:uid)
+    session.delete(:"role_#{session[:org_uid]}")
+    session.delete(:org_uid)
     @current_user = nil
 
     redirect_to root_url
