c1428bb3d7b6f9a22b9f640f0731fd8806e2b18f
Pierre Berard <pierre.berard@maestrano.com>
2015-11-21 11:07:26 +0100

    Implements maestrano webhooks

--- app/controllers/maestrano/account/groups_controller.rb
+++ app/controllers/maestrano/account/groups_controller.rb
@@ -1,10 +1,10 @@
 class Maestrano::Account::GroupsController < Maestrano::Rails::WebHookController
-  
+
   # DELETE /maestrano/account/groups/cld-1
   # Delete an entire group
   def destroy
     group_uid = params[:id]
-    
+
     # Perform deletion steps here
     # --
     # If you need to perform a final checkout
@@ -15,22 +15,22 @@ class Maestrano::Account::GroupsController < Maestrano::Rails::WebHookController
     # that group
     # --
     # E.g:
-    # organization = Organization.find_by_provider_and_uid('maestrano',group_uid)
+    organization = Organization.find_by_provider_and_uid('maestrano',group_uid)
     #
     # amount_cents = organization.calculate_total_due_remaining
     # Maestrano::Account::Bill.create({
-    #   group_id: group_uid, 
-    #   price_cents: amount_cents, 
+    #   group_id: group_uid,
+    #   price_cents: amount_cents,
     #   description: "Final Payout"
     # })
-    # 
+    #
     # if Maestrano.param('sso.creation_mode') == 'virtual'
     #   organization.members.where(provider:'maestrano').each do |user|
     #   user.destroy
     # end
     #
-    # organization.destroy
-    # render json: {success: true}
+    organization.destroy
+    render json: {success: true}
     #
   end
 end
