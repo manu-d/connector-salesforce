55d870daa06463deb750f3c7ed81db0663ba2ef0
bruno <bruno.chauvet@maestrano.com>
2016-04-06 02:05:25 +1000

    set protocol to https

--- config/initializers/maestrano.rb
+++ config/initializers/maestrano.rb
@@ -233,7 +233,7 @@ Maestrano['nab-uat'].configure do |config|
   if Rails.env.development?
     config.app.host = 'http://localhost:3001'
   else
-    config.app.host = 'http://connector-salesforce-uat.herokuapp.com'
+    config.app.host = 'https://connector-salesforce-uat.herokuapp.com'
   end
 
   config.api.host = 'https://api-hub-uat.bio.cd.non.whu.nab.com.au'
@@ -284,7 +284,7 @@ Maestrano['nab-production'].configure do |config|
   if Rails.env.development?
     config.app.host = 'http://localhost:3001'
   else
-    config.app.host = 'http://connector-salesforce.herokuapp.com'
+    config.app.host = 'https://connector-salesforce.herokuapp.com'
   end
 
   config.api.host = 'https://api-hub.bio.nab.com.au'
