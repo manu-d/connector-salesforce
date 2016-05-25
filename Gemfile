839c256c61fe4f2b4dac11a0bd7f21930f1eb7c6
Pierre Berard <pierre.berard@maestrano.com>
2016-05-25 15:35:50 +0100

    Revert attr encrypted

--- Gemfile
+++ Gemfile
@@ -10,7 +10,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'maestrano-connector-rails', git: 'https://github.com/Berardpi/maestrano-connector-rails', branch: '1.0.0'
-gem 'attr_encrypted', '~> 3.0.0'
 gem 'config'
 
 gem 'restforce'
--- db/migrate/20160525104742_add_encryption_on_oauth_keys.maestrano_connector_rails_engine.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from maestrano_connector_rails_engine (originally 20160524112054)
-class AddEncryptionOnOauthKeys < ActiveRecord::Migration
-  def change
-    rename_column :organizations, :oauth_token, :encrypted_oauth_token
-    add_column :organizations, :encrypted_oauth_token_iv, :string
-    rename_column :organizations, :refresh_token, :encrypted_refresh_token
-    add_column :organizations, :encrypted_refresh_token_iv, :string
-  end
-end
