6c9e8a99e7a16e5b4ce735239b5903affcf0b8fe
marcocode <admin@hecticmojo.com>
2017-02-03 17:22:46 +0000

    Fixes randomly generated stubs

--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -24,9 +24,10 @@ describe Maestrano::Connector::Rails::Entity do
     let(:opts) { {} }
     subject { Maestrano::Connector::Rails::Entity.new(organization, nil, external_client, opts) }
     let(:external_name) { 'external_name' }
-    before {
+
+    before(:each) do
       allow(external_client).to receive(:describe).and_return({'fields' => [{'name' => 'Id'}, {'name' => 'LastName'}]})
-    }
+    end
 
     describe 'get_external_entities' do
       context 'with full sync option' do
@@ -45,11 +46,21 @@ describe Maestrano::Connector::Rails::Entity do
           end
         end
 
-        context 'with a last sync' do
+        context 'with a last sync', :skip_before do
           let(:last_sync) { create(:synchronization, updated_at: 1.hour.ago) }
 
           it 'uses get updated' do
+
             Timecop.freeze(Date.today) do
+              allow(SecureRandom).to receive(:uuid).and_return('4d33acc5-3448-46d4-ad94-c2b37630xx9x', '4d33acc5-3448-46d4-ad94-c2b37630xx9y')
+              # stub_request(:get, "#{Maestrano['local'].param('api.host')}/api/v1/account/groups/").to_return({status: 200, body: "{}", headers: {}})
+              stub_request(:get, "https://maestrano.com/api/v1/account/groups/4d33acc5-3448-46d4-ad94-c2b37630xx9x")
+                .with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic Og==', 'User-Agent'=>'Ruby'})
+                .to_return({status: 200, body: "{}", headers: {}})
+              stub_request(:get, "https://maestrano.com/api/v1/account/groups/4d33acc5-3448-46d4-ad94-c2b37630xx9y")
+                .with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic Og==', 'User-Agent'=>'Ruby'})
+                .to_return({status: 200, body: "{}", headers: {}})
+
               allow(external_client).to receive(:get_updated).and_return({'ids' => []})
               expect(external_client).to receive(:get_updated).with(external_name, last_sync.updated_at - 2.minutes, Time.now + 2.minutes)
               subject.get_external_entities(external_name, last_sync.updated_at)
@@ -57,6 +68,15 @@ describe Maestrano::Connector::Rails::Entity do
           end
 
           it 'calls find on received ids' do
+            allow(SecureRandom).to receive(:uuid).and_return('4d33acc5-3448-46d4-ad94-c2b37630xx9w', '4d33acc5-3448-46d4-ad94-c2b37630xx9z')
+            # stub_request(:get, "#{Maestrano['local'].param('api.host')}/api/v1/account/groups/").to_return({status: 200, body: "{}", headers: {}})
+            stub_request(:get, "https://maestrano.com/api/v1/account/groups/4d33acc5-3448-46d4-ad94-c2b37630xx9w")
+              .with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic Og==', 'User-Agent'=>'Ruby'})
+              .to_return({status: 200, body: "{}", headers: {}})
+            stub_request(:get, "https://maestrano.com/api/v1/account/groups/4d33acc5-3448-46d4-ad94-c2b37630xx9z")
+              .with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic Og==', 'User-Agent'=>'Ruby'})
+              .to_return({status: 200, body: "{}", headers: {}})
+
             allow(external_client).to receive(:get_updated).and_return({'ids' => [3, 5, 6]})
             expect(external_client).to receive(:find).with(external_name, 3)
             expect(external_client).to receive(:find).with(external_name, 5)
@@ -65,6 +85,15 @@ describe Maestrano::Connector::Rails::Entity do
           end
 
           it 'returns the entities' do
+            allow(SecureRandom).to receive(:uuid).and_return('4d33acc5-3448-46d4-ad94-c2b37630yy9w', '4d33acc5-3448-46d4-ad94-c2b37630yy9z')
+            # stub_request(:get, "#{Maestrano['local'].param('api.host')}/api/v1/account/groups/").to_return({status: 200, body: "{}", headers: {}})
+            stub_request(:get, "https://maestrano.com/api/v1/account/groups/4d33acc5-3448-46d4-ad94-c2b37630yy9w")
+              .with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic Og==', 'User-Agent'=>'Ruby'})
+              .to_return({status: 200, body: "{}", headers: {}})
+            stub_request(:get, "https://maestrano.com/api/v1/account/groups/4d33acc5-3448-46d4-ad94-c2b37630yy9z")
+              .with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic Og==', 'User-Agent'=>'Ruby'})
+              .to_return({status: 200, body: "{}", headers: {}})
+
             allow(external_client).to receive(:get_updated).and_return({'ids' => [3, 5, 6]})
             allow(external_client).to receive(:find).and_return({'FirstName' => 'John'})
             expect(subject.get_external_entities(external_name, last_sync.updated_at)).to eql([{'FirstName' => 'John'}, {'FirstName' => 'John'}, {'FirstName' => 'John'}])
