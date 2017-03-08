cfd236d8213353345f9284e4b46bb5e42c1de7e5
Emmanuel Darnis <emmanuel.darnis@maestrano.com>
2017-03-08 14:45:47 +1100

    Re-enable historical data

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,6 +2,7 @@ function historicalDataDisplay()
 {
   if (document.getElementById('historical-data').checked)
   {
+	  $('#myModal').modal('show');
       document.getElementById('historical-data-display-checked').style.display = 'block';
       document.getElementById('historical-data-display-unchecked').style.display = 'none';
   } else {
@@ -10,6 +11,24 @@ function historicalDataDisplay()
   }
 }
 
+var checkHistorical
+
+function closeModal(sender)
+{
+  checkHistorical = sender.id == 'confirm'
+  $('#myModal').modal('hide');
+}
+
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(document).ready(function(){
+    $("#myModal").on('hidden.bs.modal', function (e) {
+      if (!checkHistorical) {
+		document.getElementById('historical-data').checked = false
+		historicalDataDisplay()
+      }
+      checkHistorical = false
+    });
+});
